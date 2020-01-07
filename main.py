f7610837d2e1501ceac59e6f5a8a30c2eb49971b
fpopescu <popescu01@gmail.com>
2020-01-07 10:50:20 -0600

    another desperate attempt

--- main.py
+++ main.py
@@ -64,6 +64,8 @@ def incomplete_tasks():
 # -----------------------------------------
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
 
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
+    
